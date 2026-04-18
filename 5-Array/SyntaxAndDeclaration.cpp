9fcff6c708122e7d6dec4788ceec6b7bef8999ab
S-Mauryaa <github.saurabhmaurya@gmail.com>
2026-04-19 00:00:47 +0530

    Update

---
+++ 5-Array/SyntaxAndDeclaration.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){   
+    int arr[9];
+    cout<<"enter the number";
+     for(int i=0;i<=8;i++){
+        cin>>arr[i];
+     }
+     for(int i=0;i<=8;i++){
+        cout<<arr[i];
+     }
+        }
