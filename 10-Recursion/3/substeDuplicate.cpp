ae515c4d0520ce276034fc1a354a3386c878da5f
S-Mauryaa <github.saurabhmaurya@gmail.com>
2026-09-01 22:34:22 +0530

    new

--- 10-Recursion/3/substeDuplicate.cpp
+++ 10-Recursion/3/substeDuplicate.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include <vector>
 using namespace std;
-void storeSubset(string ans, string original, vector<string> &v)
+void storeSubset(string ans, string original, vector<string> &v,bool flag)
 {
     if (original == "")
     {
@@ -17,7 +17,7 @@ int main()
 {
     string str = "aab";
     vector<string> v;
-    storeSubset("", str, v);
+    storeSubset("", str, v,true);
     for (int i = 0; i < v.size(); i++)
     {
         cout << v[i] << endl;
